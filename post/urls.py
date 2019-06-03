fec55a61e7c6c4f192b0f22efc78a787bd579c57
furkanonder <furkantahaonder@gmail.com>
2019-06-03 23:56:53 +0300

    Some functions were rewritten as class-based.

--- post/urls.py
+++ post/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path,re_path
 
-from .views import post_list,post_create,post_detail,post_update,post_delete,UserProfile
+from .views import post_create,post_detail,post_update,PostList,PostDelete,UserProfile
 
 app_name = 'post'
 urlpatterns = [
-	path('', post_list, name='list'),
     path('create/', post_create,name='create'),
     path('<slug:slug>/', post_detail, name='detail'),
     path('<slug:slug>/update/', post_update, name='update'),
-    path('<slug:slug>/delete/', post_delete, name='delete'),
+   	path('', view=PostList.as_view(), name='list'),
+    path('<slug:slug>/delete/', view=PostDelete.as_view(), name='delete'),
     path('@<username>/', view=UserProfile.as_view(), name="userPost"),
 ]
--- post/views.py
+++ post/views.py
@@ -2,7 +2,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.contenttypes.models import ContentType
-from django.views.generic import DetailView
+from django.views.generic import DetailView,DeleteView,UpdateView
 from django.contrib.auth.models import User
 from comments.forms import CommentForm
 from comments.models import Comment
@@ -14,140 +14,154 @@ from .forms import PostForm
 from .models import Post
 
 def post_create(request):
-	form = PostForm(request.POST or None, request.FILES or None)
-	if form.is_valid():
-		instance = form.save(commit=False)
-		instance.user = request.user
-		instance.save()
-		messages.success(request, "Successfully Created")
-		return HttpResponseRedirect(instance.get_absolute_url())
+    form = PostForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.user = request.user
+        instance.save()
+        messages.success(request, "Successfully Created")
+        return HttpResponseRedirect(instance.get_absolute_url())
 
-	context = dict(form=form)	
-	return render(request, "post/post_form.html", context)
+    context = dict(form=form)   
+    return render(request, "post/post_form.html", context)
 
 
 def post_detail(request, slug=None):
-	instance = get_object_or_404(Post, slug=slug)
+    instance = get_object_or_404(Post, slug=slug)
 
-	initial_data = {
-		"content_type": instance.get_content_type,
-		"object_id": instance.id
-	}
-	form = CommentForm(request.POST or None, initial=initial_data)
-	if form.is_valid():
-		c_type = form.cleaned_data.get("content_type")
-		content_type = ContentType.objects.get(model=c_type)
-		obj_id = form.cleaned_data.get('object_id')
-		content_data = form.cleaned_data.get("content")
-		parent_obj = None
-		try:
-			parent_id = int(request.POST.get("parent_id"))
-		except:
-			parent_id = None
+    initial_data = {
+        "content_type": instance.get_content_type,
+        "object_id": instance.id
+    }
+    form = CommentForm(request.POST or None, initial=initial_data)
+    if form.is_valid():
+        c_type = form.cleaned_data.get("content_type")
+        content_type = ContentType.objects.get(model=c_type)
+        obj_id = form.cleaned_data.get('object_id')
+        content_data = form.cleaned_data.get("content")
+        parent_obj = None
+        try:
+            parent_id = int(request.POST.get("parent_id"))
+        except:
+            parent_id = None
 
-		if parent_id:
-			parent_qs = Comment.objects.filter(id=parent_id)
-			if parent_qs.exists() and parent_qs.count() == 1:
-				parent_obj = parent_qs.first()
-
-
-		new_comment, created = Comment.objects.get_or_create(
-			user = request.user,
-			content_type= content_type,
-			object_id = obj_id,
-			content = content_data,
-			parent = parent_obj,
-		)
-		return HttpResponseRedirect(new_comment.content_object.get_absolute_url())
+        if parent_id:
+            parent_qs = Comment.objects.filter(id=parent_id)
+            if parent_qs.exists() and parent_qs.count() == 1:
+                parent_obj = parent_qs.first()
 
 
-	comments = instance.comments
-	context = {
-		"title": instance.title,
-		"instance": instance,
-		"comments": comments,
-		"comment_form":form,
-	}
-	return render(request, "post/post_detail.html", context)
+        new_comment, created = Comment.objects.get_or_create(
+            user = request.user,
+            content_type= content_type,
+            object_id = obj_id,
+            content = content_data,
+            parent = parent_obj,
+        )
+        return HttpResponseRedirect(new_comment.content_object.get_absolute_url())
 
 
-def post_list(request):
-	today = timezone.now().date()
-	queryset_list = Post.objects.active() #.order_by("-timestamp")
-	queryset_list = Post.objects.all()
-	
-	query = request.GET.get("q")
-	if query:
-		queryset_list = queryset_list.filter(
-			Q(title__icontains=query)|
-			Q(content__icontains=query)|
-			Q(user__first_name__icontains=query) |
-			Q(user__last_name__icontains=query)
-		).distinct()
-
-	paginator = Paginator(queryset_list, 8) # Show 25 contacts per page
-	page_request_var = "page"
-	page = request.GET.get(page_request_var)
-	try:
-		queryset = paginator.page(page)
-	except PageNotAnInteger:
-		# If page is not an integer, deliver first page.
-		queryset = paginator.page(1)
-	except EmptyPage:
-		# If page is out of range (e.g. 9999), deliver last page of results.
-		queryset = paginator.page(paginator.num_pages)
-
-	context = {
-		"object_list": queryset, 
-		"title": "List",
-		"page_request_var": page_request_var,
-		"today": today,
-	}
-	return render(request, "post/post_list.html", context)
+    comments = instance.comments
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "comments": comments,
+        "comment_form":form,
+    }
+    return render(request, "post/post_detail.html", context)
 
 
 def post_update(request, slug=None):
-	instance = get_object_or_404(Post, slug=slug)
-	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
-	if form.is_valid():
-		instance = form.save(commit=False)
-		instance.save()
-		messages.success(request, "<a href='#'>Item</a> Saved", extra_tags='html_safe')
-		return HttpResponseRedirect(instance.get_absolute_url())
+    instance = get_object_or_404(Post, slug=slug)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 
-	context = {
-		"title": instance.title,
-		"instance": instance,
-		"form":form,
-	}
-	return render(request, "post/post_form.html", context)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
 
-def post_delete(request, slug=None):
-	instance = get_object_or_404(Post, slug=slug)
-	instance.delete()
-	messages.success(request, "Successfully deleted")
-	return redirect("posts:list")
+
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form":form,
+    }
+
+    return render(request, "post/post_form.html", context)
+
+
+class PostList(DetailView):
+    template_name = "post/post_list.html"
+    model = Post
+
+    def get(self, request):
+        today = timezone.now().date()
+        #.order_by("-timestamp")
+        queryset_list = Post.objects.active() 
+        queryset_list = Post.objects.all()
+        query = request.GET.get("q")
+
+        if query:
+            queryset_list = queryset_list.filter(
+                Q(title__icontains=query)|
+                Q(content__icontains=query)|
+                Q(user__first_name__icontains=query) |
+                Q(user__last_name__icontains=query)
+            ).distinct()
+
+        paginator = Paginator(queryset_list, 8) # Show 25 contacts per page
+        page_request_var = "page"
+        page = request.GET.get(page_request_var)
+
+        try:
+            queryset = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            queryset = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            queryset = paginator.page(paginator.num_pages)
+
+        context = {
+            "object_list": queryset, 
+            "title": "List",
+            "page_request_var": page_request_var,
+            "today": today,
+        }
+
+        return render(request, self.template_name, context)
+
+
+class PostDelete(DeleteView):
+    model = Post
+
+    def get(self, request, slug):
+        instance = get_object_or_404(Post, slug=slug)
+        instance.delete()
+        messages.success(request, "Successfully deleted")
+        return redirect("posts:list")
 
 
 class UserProfile(DetailView):
-	template_name = "post/user_profile.html"
-	model = Post, User
+    template_name = "post/user_profile.html"
+    model = Post, User
 
-	def get(self, request, username):
-		user = User.objects.get(username=username)
-		post_list = Post.objects.filter(user=user.pk)
-		paginator = Paginator(post_list, 10)
-		page = request.GET.get('page')
+    def get(self, request, username):
+        user = User.objects.get(username=username)
+        post_list = Post.objects.filter(user=user.pk)
+        paginator = Paginator(post_list, 10)
+        page = request.GET.get('page')
 
-		try:
-			Posts = paginator.page(page)
-		except PageNotAnInteger:
-			Posts = paginator.page(1)
-		except EmptyPage:
-			Posts = paginator.page(paginator.num_pages)
+        try:
+            Posts = paginator.page(page)
+        except PageNotAnInteger:
+            Posts = paginator.page(1)
+        except EmptyPage:
+            Posts = paginator.page(paginator.num_pages)
 
-		if post_list == []:
-			messages.add_message(request, messages.WARNING, "The user does not have a share!")
+        if post_list == []:
+            messages.add_message(request, messages.WARNING,\
+             "The user does not have a share!")
 
-		context = dict(Posts=Posts, user=user)
-		return render(request, self.template_name, context)	
+        context = dict(Posts=Posts, user=user)
+        return render(request, self.template_name, context) 
